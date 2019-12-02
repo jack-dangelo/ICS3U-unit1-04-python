43664fb22f2ff4d6c26f7a72943fb0d3fee1cad0
jack-dangelo <54954654+jack-dangelo@users.noreply.github.com>
2019-12-02 13:05:03 -0500

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Jack D'angelo
+# Created on: September 2019
+# This program calculates the area and perimeter of a rectangle
+
+
+def main():
+    print("If rectangle has the dimensions")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(3*5))
+    print("Perimeter is {}cm".format(2*(3+5)))
+
+
+if __name__ == "__main__":
+    main()
